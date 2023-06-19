906051a44a3a6e5666219a9f8ba594c0be8432c3
Maria Agustina Avagnina <avagnina.agus@yahoo.com.ar>
2023-06-19 05:08:45 -0300

    agrego funciones

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
 let nombreUsuario = prompt("Ingrese nombre de usuario");
-if (nombreUsuario === "") {
-  alert("Registrate aca");
-  nombreUsuario = prompt("Escriba su email");
-  prompt("Elija una contraseña");
-  alert("Te mandamos un correo a tu email para verificar la cuenta");
-} else {
-  prompt("Contraseña");
-  alert("Bienvenido/a " + nombreUsuario);
+function saludar(nombre) {
+  if (nombreUsuario === "") {
+    alert("Registrate aca");
+    nombreUsuario = prompt("Escriba su email");
+    prompt("Elija una contraseña");
+    alert("Te mandamos un correo a tu email para verificar la cuenta");
+  } else {
+    prompt("Contraseña");
+    alert("Bienvenido/a " + nombreUsuario);
+  }
 }
+saludar(nombreUsuario);
 
 let banco = prompt("Ingresa tu banco");
 for (let cantidadCuotas = 2; cantidadCuotas <= 6; cantidadCuotas++) {
-  if (banco === "Galicia") {
+  if (banco === "Galicia" || banco === "galicia") {
     alert("Tenes hasta 12 cuotas sin interes");
     break;
+  } else if (banco === "Santander" || banco === "santander") {
+    alert("Tenes hasta 6 cuotas sin interes");
+    break;
+  } else {
+    let resultado = cantidadCuotas * 4;
+    alert(
+      "En " + cantidadCuotas + " cuotas, el recargo es de: " + resultado + "%"
+    );
   }
-  let resultado = cantidadCuotas * 4;
-  alert(
-    "En " + cantidadCuotas + " cuotas, el recargo es de: " + resultado + "%"
-  );
 }
 
 let barrio = prompt("Ingrese su barrio");
@@ -61,3 +68,11 @@ do {
   cp = Number(prompt("Codigo postal"));
   esc = prompt("Escriba ESC para finalizar");
 } while (esc !== "ESC");
+
+function sumar(zapatillas, sandalias) {
+  alert("Agregaste zapatillas al carrito: $" + zapatillas);
+  alert("Agregaste sandalias al carrito: $" + sandalias);
+  let resultado = zapatillas + sandalias;
+  alert("Usted debe abonar: $" + resultado);
+}
+sumar(5000, 3000);
