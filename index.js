e95b634f4761bfa92d021ddb0c907e4e0857ce9e
Maria Agustina Avagnina <avagnina.agus@yahoo.com.ar>
2023-07-10 21:30:04 -0300

    edito para segunda entrega

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let nombreUsuario = prompt("Ingrese nombre de usuario");
+
 if (nombreUsuario === "") {
   alert("Registrate aca");
   let email = prompt("Escriba su email");
@@ -8,7 +9,7 @@ if (nombreUsuario === "") {
   );
 } else {
   contraseña = prompt("Contraseña");
-  alert("Bienvenido/a " + nombreUsuario);
+  alert("Bienvenido/a " + nombreUsuario.toLocaleUpperCase());
 }
 
 let banco = prompt("Ingresa tu banco");
@@ -68,10 +69,72 @@ do {
   esc = prompt("Escriba ESC para finalizar");
 } while (esc !== "ESC");
 
-function sumar(zapatillas, sandalias) {
-  alert("Agregaste zapatillas al carrito: $" + zapatillas);
-  alert("Agregaste sandalias al carrito: $" + sandalias);
-  let resultado = zapatillas + sandalias;
-  alert("Usted debe abonar: $" + resultado);
+class Usuario {
+  constructor(nombre, apellido, email, contraseña) {
+    (this.nombre = nombre.toUpperCase()),
+      (this.apellido = apellido.toUpperCase()),
+      (this.email = email),
+      (this.contraseña = contraseña);
+  }
+  hablar() {
+    console.log("HOLA " + this.nombre + " " + this.apellido);
+  }
 }
-sumar(5000, 3000);
+
+const usuario1 = new Usuario(
+  "Agustina",
+  "Avagnina",
+  "avag@gmail.com",
+  "avagagus1234"
+);
+
+const usuario2 = new Usuario(
+  "Ezequiel",
+  "Gonzalez",
+  "ezegonz@gmail.com",
+  "gonzalez1234"
+);
+
+usuario1.hablar();
+usuario2.hablar();
+
+class Producto {
+  constructor(nombre, precio) {
+    this.nombre = nombre.toUpperCase();
+    this.precio = parseFloat(precio);
+    this.vendido = false;
+  }
+  sumarIva() {
+    this.precio = this.precio * 1.21;
+  }
+  descuento() {
+    this.precio = this.precio * 0.8;
+  }
+  vender() {
+    this.vendido = true;
+  }
+}
+
+const producto1 = new Producto("Zapatillas", 5000);
+const producto2 = new Producto("Sandalias", 2000);
+const producto3 = new Producto("Botas", 8000);
+
+producto1.sumarIva();
+producto2.sumarIva();
+producto3.descuento();
+producto3.vender();
+
+console.log(producto3);
+
+const listaProductos = [];
+const cantidad = Number(
+  prompt("Ingrese cuantos productos vas a agregar al carrito")
+);
+for (index = 0; index < cantidad; index++) {
+  do {
+    let entrada = prompt("Ingrese un producto para agregar al carrito");
+    listaProductos.push(entrada);
+  } while (listaProductos == !"");
+
+  alert("Usted agrego " + listaProductos.join("-") + " al carrito");
+}
