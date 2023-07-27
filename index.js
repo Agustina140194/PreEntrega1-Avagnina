7eaa40bc15dc8c309d10162541e3e35e766269bf
Maria Agustina Avagnina <avagnina.agus@yahoo.com.ar>
2023-07-27 05:36:52 -0300

    creo el carrito

--- index.js
+++ index.js
@@ -1,127 +1,70 @@
-const usuarios = [];
-
-let nombreUsuario = prompt("Ingrese nombre de usuario");
-
-if (nombreUsuario === "") {
-  alert("Registrate aca");
-  let email = prompt("Escriba su email");
-  let contraseña = prompt("Elija una contraseña");
-  alert(
-    "Te mandamos un correo a tu email a " + email + " para verificar la cuenta"
-  );
-} else {
-  contraseña = prompt("Contraseña");
-  alert("Bienvenido/a " + nombreUsuario.toLocaleUpperCase());
-}
-
-let nuevoUsuario = {
-  nombreUsuario: nombreUsuario,
-  contraseña: contraseña,
-};
-
-usuarios.push(nuevoUsuario);
-
-console.log(usuarios);
-
-let banco = prompt("Ingresa tu banco");
-for (let cantidadCuotas = 2; cantidadCuotas <= 6; cantidadCuotas++) {
-  if (banco === "Galicia" || banco === "galicia") {
-    alert("Tenes hasta 12 cuotas sin interes");
-    break;
-  } else if (banco === "Santander" || banco === "santander") {
-    alert("Tenes hasta 6 cuotas sin interes");
-    break;
-  } else {
-    let resultado = cantidadCuotas * 4;
-    alert(
-      "En " + cantidadCuotas + " cuotas, el recargo es de: " + resultado + "%"
-    );
-  }
-}
-
-let costosEnvios = [
-  { barrio: "Recoleta", envio: 300 },
-  { barrio: "Palermo", envio: 350 },
-  { barrio: "Colegiales", envio: 400 },
-  { barrio: "Belgrano", envio: 450 },
-  { barrio: "Retiro", envio: 500 },
-  { barrio: "Saavedra", envio: 550 },
+const productos = [
+  { id: "producto1", nombre: "Camelia", precio: 5000 },
+  { id: "producto2", nombre: "Bugalo", precio: 7000 },
+  { id: "producto3", nombre: "Alamo", precio: 6000 },
+  { id: "producto4", nombre: "Verbena", precio: 4000 },
 ];
 
-let barrio = prompt("Ingrese el barrio ");
+const botonesAgregar = document.getElementsByClassName("agregar-carrito");
+for (let i = 0; i < botonesAgregar.length; i++) {
+  botonesAgregar[i].addEventListener("click", (event) => {
+    const productoId = event.target.id;
+    agregarAlCarrito(productoId);
+  });
+}
 
-while (barrio !== "ESC") {
-  let encontrado = costosEnvios.find(function (elemento) {
-    return elemento.barrio === barrio;
+function agregarAlCarrito(id) {
+  let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+  const productoExistente = carrito.find((producto) => producto.id === id);
+
+  if (productoExistente) {
+    productoExistente.cantidad++;
+  } else {
+    const producto = productos.find((prod) => prod.id === id);
+    carrito.push({ ...producto, cantidad: 1 });
+  }
+
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+  actualizarCarritoHeader();
+}
+
+document.addEventListener("click", (event) => {
+  if (event.target.classList.contains("eliminar-producto")) {
+    const productoId = event.target.getAttribute("data-id");
+    eliminarDelCarrito(productoId);
+  }
+});
+
+function actualizarCarritoHeader() {
+  const carritoTotal = document.getElementById("carrito-total");
+  const carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+  let total = 0;
+  carrito.forEach((producto) => {
+    total += producto.precio * producto.cantidad;
   });
 
-  if (encontrado) {
-    alert("El costo de envio es de $" + encontrado.envio);
-  } else {
-    alert("Estamos trabajando para enviar a tu zona");
-  }
-  barrio = prompt("Ingrese otro barrio");
+  carritoTotal.textContent = `$${total}`;
+
+  mostrarCarritoContenido();
 }
 
-alert("Datos para el envio");
-do {
-  nombreApellido = prompt("Ingresa tu nombre y apellido completo: ");
-  documento = Number(prompt("Ingresa tu numero de documento"));
-  direccion = prompt("Ingresa tu direccion");
-  cp = Number(prompt("Codigo postal"));
-  esc = prompt("Escriba ESC para finalizar");
-} while (esc !== "ESC");
+function mostrarCarritoContenido() {
+  const carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+  const carritoContenido = document.getElementById("carrito-contenido");
+  let carritoHTML = "";
 
-class Producto {
-  constructor(nombre, precio) {
-    this.nombre = nombre.toUpperCase();
-    this.precio = parseFloat(precio);
-    this.vendido = false;
-  }
-  sumarIva() {
-    this.precio = this.precio * 1.21;
-  }
-  descuento() {
-    this.precio = this.precio * 0.8;
-  }
-  vender() {
-    this.vendido = true;
-  }
+  carrito.forEach((producto) => {
+    const subtotal = producto.precio * producto.cantidad;
+    carritoHTML += `
+      <div>
+        <span>${producto.nombre} - $${producto.precio} x ${producto.cantidad} = $${subtotal}</span>
+        <button class="eliminar-producto" data-id="${producto.id}">Eliminar</button>
+      </div>
+    `;
+  });
+
+  carritoContenido.innerHTML = carritoHTML;
 }
 
-const productos = [
-  new Producto("Zapatilla de cuero", 5000),
-  new Producto("Zapatilla de tela", 4000),
-  new Producto("Sandalia", 3000),
-  new Producto("Bota", 8000),
-  new Producto("Ojota", 2000),
-];
-
-const zapas = productos.filter((producto) =>
-  producto.nombre.includes("Zapatilla")
-);
-console.log(zapas);
-
-let productoElegido = prompt("Que producto estas buscando?");
-let productoEncontrado = productos.find(
-  (producto) => producto.nombre === productoElegido
-);
-if (productoEncontrado) {
-  console.log(productoEncontrado);
-} else {
-  console.log("El producto no se encontro");
-}
-
-const listaProductos = [];
-const cantidad = Number(
-  prompt("Ingrese cuantos productos vas a agregar al carrito")
-);
-for (let index = 0; index < cantidad; index++) {
-  let entrada = prompt("Ingrese un producto para agregar al carrito");
-  if (entrada === "ESC") {
-    break;
-  }
-  listaProductos.push(entrada);
-}
-
-alert("Usted agrego " + listaProductos.join("-") + " al carrito");
+actualizarCarritoHeader();
